29f8d7bce474e6f724878658ca3c6480a5cbb411
Christian Engel <engel.christian1994@gmail.com>
2020-05-06 18:30:24 +0200

    Add more dethlify related stuff

--- contracts/Utils.sol
+++ contracts/Utils.sol
@@ -1,4 +1,5 @@
 pragma solidity 0.6.6;
+pragma experimental ABIEncoderV2;
 
 
 /**
@@ -80,6 +81,85 @@ contract Utils {
     }
     return borrowRates;
   }
+
+  /**
+   * @dev Get DSR contract DAI balances for a list of users
+   *
+   * @param _users array of users that have DAI in the DSR contract
+   * @param _pot the MCD_POT contract address
+   */
+  function getDSRBalances(address[] memory _users, address _pot)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    uint256[] memory balances = new uint256[](_users.length);
+
+    for (uint256 i = 0; i < balances.length; i++) {
+      balances[i] = Dethlify(_users[i]).getDSRBalance(_pot);
+    }
+    return balances;
+  }
+
+  /**
+   * @dev Get the last callbacks for a list of dethlify contracts
+   *
+   * @param _contracts the contract addresses
+   */
+  function getLasts(address[] memory _contracts) public view returns (uint256[] memory) {
+    uint256[] memory lasts = new uint256[](_contracts.length);
+    for (uint256 i = 0; i < _contracts.length; i++) {
+      lasts[i] = Dethlify(_contracts[i]).last();
+    }
+    return lasts;
+  }
+
+  /**
+   * @dev Get the lock period for a list of dethlify contracts
+   *
+   * @param _contracts the contract addresses
+   */
+  function getLocks(address[] memory _contracts) public view returns (uint256[] memory) {
+    uint256[] memory locks = new uint256[](_contracts.length);
+    for (uint256 i = 0; i < _contracts.length; i++) {
+      locks[i] = Dethlify(_contracts[i]).lock();
+    }
+    return locks;
+  }
+
+  /**
+   * @dev Get the versions for a list of dethlify contracts
+   *
+   * @param _contracts the contract addresses
+   */
+  function getVersions(address[] memory _contracts) public view returns (string[] memory) {
+    string[] memory versions = new string[](_contracts.length);
+    for (uint256 i = 0; i < _contracts.length; i++) {
+      versions[i] = Dethlify(_contracts[i]).version();
+    }
+    return versions;
+  }
+
+  /**
+   * @dev Get the token distributions for a list of heirs
+   *
+   * @param _heirs list of heirs
+   * @param _tokens list of tokens
+   * @param _contract the contract addresses
+   */
+  function getDistributions(
+    address[] memory _heirs,
+    address[] memory _tokens,
+    address _contract
+  ) public view returns (string[] memory) {
+    string[] memory distributions = new string[](_heirs.length * _tokens.length);
+    for (uint256 i = 0; i < _tokens.length; i++) {
+      for (uint256 j = 0; j < _heirs.length; j++) {
+        distributions[j + _heirs.length * i] = Dethlify(_contract).tokenDistribution(_tokens[i], _heirs[j]); // prettier-ignore
+      }
+    }
+    return distributions;
+  }
 }
 
 
@@ -96,4 +176,21 @@ interface MultiToken {
   function balanceOfUnderlying(address account) external returns (uint256);
 
   function balanceOf(address owner) external view returns (uint256);
+}
+
+
+/**
+ + @title Dethlify
+ * @dev Interface for dethlify smart contracts
+ */
+interface Dethlify {
+  function getDSRBalance(address _pot) external view returns (uint256);
+
+  function lock() external view returns (uint256);
+
+  function last() external view returns (uint256);
+
+  function version() external view returns (string memory);
+
+  function tokenDistribution(address token, address heir) external view returns (string memory);
 }
